dc88346f019841c5db93469e47921e7812538d59
Dan Creswell <dan.creswell@gmail.com>
2013-02-15 20:23:49 +0000

    Tests for HttpServer and InProcessPeer

---
+++ src/test/java/org/dancres/peers/primitives/HttpServerTest.java
@@ -0,0 +1,42 @@
+package org.dancres.peers.primitives;
+
+import com.ning.http.client.AsyncCompletionHandler;
+import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.Response;
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class HttpServerTest {
+    private boolean _success = false;
+
+    @Test
+    public void test() throws Exception {
+        HttpServer myServer = new HttpServer(new InetSocketAddress("localhost", 8081));
+
+        // Test we get a basic response
+        AsyncHttpClient myClient = new AsyncHttpClient();
+
+        try {
+            myClient.prepareGet(myServer.getBase().toString()).execute(new AsyncCompletionHandler<Response>() {
+
+                public Response onCompleted(Response aResponse) throws Exception {
+                    String myBody = aResponse.getResponseBody();
+                    Assert.assertNotNull(myBody);
+                    Assert.assertNotSame(0, myBody.length());
+
+                    _success = true;
+
+                    return aResponse;
+                }
+            });
+        } catch (IOException anIOE) {
+        }
+
+        Thread.sleep(1000);
+
+        Assert.assertTrue(_success);
+    }
+}
---
+++ src/test/java/org/dancres/peers/primitives/InProcessPeerTest.java
@@ -0,0 +1,66 @@
+package org.dancres.peers.primitives;
+
+import com.ning.http.client.AsyncCompletionHandler;
+import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.Response;
+import junit.framework.Assert;
+import org.dancres.peers.Peer;
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.handler.codec.http.HttpRequest;
+import org.jboss.netty.handler.codec.http.HttpResponse;
+import org.jboss.netty.handler.codec.http.HttpResponseStatus;
+import org.jboss.netty.util.CharsetUtil;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.Timer;
+
+public class InProcessPeerTest {
+    private static final String TEST_RESPONSE = "test response";
+
+    private boolean _success = false;
+
+    @Test
+    public void test() throws Exception {
+        HttpServer myServer = new HttpServer(new InetSocketAddress("localhost", 8081));
+        AsyncHttpClient myClient = new AsyncHttpClient();
+
+        Peer myPeer1 = new InProcessPeer(myServer, myClient, "/peer1", new Timer());
+
+        Peer.ServiceDispatcher myDispatcher = new Dispatcher();
+
+        myPeer1.add("/test", myDispatcher);
+
+        try {
+            myClient.prepareGet(myPeer1.getAddress().toString() + "/test").execute(
+                    new AsyncCompletionHandler<Response>() {
+
+                        public Response onCompleted(Response aResponse) throws Exception {
+                            String myBody = aResponse.getResponseBody();
+                            Assert.assertNotNull(myBody);
+                            Assert.assertNotSame(0, myBody.length());
+                            Assert.assertEquals(myBody, TEST_RESPONSE);
+
+                            return aResponse;
+                        }
+                    });
+        } catch (IOException anIOE) {
+        }
+
+        Thread.sleep(1000);
+
+        Assert.assertTrue(_success);
+    }
+
+    class Dispatcher implements Peer.ServiceDispatcher {
+
+        @Override
+        public void dispatch(String aServicePath, HttpRequest aRequest, HttpResponse aResponse) {
+            _success = true;
+
+            aResponse.setContent(ChannelBuffers.copiedBuffer(TEST_RESPONSE, CharsetUtil.UTF_8));
+            aResponse.setStatus(HttpResponseStatus.OK);
+        }
+    }
+}
