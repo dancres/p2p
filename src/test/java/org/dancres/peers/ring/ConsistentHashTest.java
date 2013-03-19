3ccbb79f31ff8a1a0e177d25a6b08a74210bdf9b
Dan Creswell <dan.creswell@gmail.com>
2013-03-19 08:09:22 -0500

    Update test to cope with API changes in ConsistentHash and perform a stability test (Ring takes).

--- src/test/java/org/dancres/peers/ring/ConsistentHashTest.java
+++ src/test/java/org/dancres/peers/ring/ConsistentHashTest.java
@@ -84,8 +84,8 @@ public class ConsistentHashTest {
         Assert.assertEquals(0, myPeer1RejectCount.get());
         Assert.assertEquals(1, myPeer2RejectCount.get());
 
-        Assert.assertEquals(1, myRing1.getCurrentPositions().getPositions().size());
-        Assert.assertEquals(0, myRing2.getCurrentPositions().getPositions().size());
+        Assert.assertEquals(1, myRing1.getPeerPositions().getPositions().size());
+        Assert.assertEquals(0, myRing2.getPeerPositions().getPositions().size());
 
         Assert.assertEquals(1, myRing1.getCurrentRing().size());
         Assert.assertEquals(1, myRing2.getCurrentRing().size());
@@ -101,11 +101,11 @@ public class ConsistentHashTest {
             _count = aCount;
         }
 
-        public void newNeighbour(ConsistentHash.RingPosition anOwnedPosition,
+        public void newNeighbour(ConsistentHash aRing, ConsistentHash.RingPosition anOwnedPosition,
                                  ConsistentHash.RingPosition aNeighbourPosition) {
         }
 
-        public void rejected(ConsistentHash.RingPosition anOwnedPosition) {
+        public void rejected(ConsistentHash aRing, ConsistentHash.RingPosition anOwnedPosition) {
             _count.incrementAndGet();
         }
     }
@@ -203,12 +203,89 @@ public class ConsistentHashTest {
             _count = aCount;
         }
 
-        public void newNeighbour(ConsistentHash.RingPosition anOwnedPosition,
+        public void newNeighbour(ConsistentHash aRing, ConsistentHash.RingPosition anOwnedPosition,
                                  ConsistentHash.RingPosition aNeighbourPosition) {
             _count.incrementAndGet();
         }
 
-        public void rejected(ConsistentHash.RingPosition anOwnedPosition) {
+        public void rejected(ConsistentHash aRing, ConsistentHash.RingPosition anOwnedPosition) {
+        }
+    }
+
+    @Test
+    public void testGetsStable() throws Exception {
+        HttpServer myServer = new HttpServer(new InetSocketAddress("localhost", 8083));
+        AsyncHttpClient myClient = new AsyncHttpClient();
+        GossipBarrier myBarrier1 = new GossipBarrier();
+        GossipBarrier myBarrier2 = new GossipBarrier();
+
+        Peer myPeer1 = new InProcessPeer(myServer, myClient, "/peer1", new Timer());
+        Peer myPeer2 = new InProcessPeer(myServer, myClient, "/peer2", new Timer());
+
+        Set<URI> myPeers = new HashSet<URI>();
+        myPeers.add(myPeer1.getURI());
+        myPeers.add(myPeer2.getURI());
+
+        PeerSet myPeerSet = new StaticPeerSet(myPeers);
+
+        Directory myPeer1Dir = new Directory(myPeer1, myPeerSet);
+        Directory myPeer2Dir = new Directory(myPeer2, myPeerSet);
+
+        myPeer1Dir.add(myBarrier1);
+        myPeer2Dir.add(myBarrier2);
+
+        int myBarr1Curr = myBarrier1.current();
+        int myBarr2Curr = myBarrier2.current();
+
+        myPeer1Dir.start();
+
+        myBarrier1.await(myBarr1Curr);
+        myBarrier2.await(myBarr2Curr);
+
+        // Directory is stable?
+        //
+        Assert.assertEquals(2, myPeer1Dir.getDirectory().size());
+        Assert.assertEquals(2, myPeer2Dir.getDirectory().size());
+
+        ConsistentHash myRing1 = new ConsistentHash(myPeer1);
+        ConsistentHash myRing2 = new ConsistentHash(myPeer2);
+
+        myRing1.add(new StabiliserImpl());
+        myRing2.add(new StabiliserImpl());
+
+        for (int i = 0; i < 3; i++) {
+            myRing1.newPosition();
+            myRing2.newPosition();
+        }
+
+        // Allow some gossip time so that this ring position has "taken" across the cluster of peers
+        //
+        for (int i = 0; i < 10; i++) {
+            myBarrier1.await(myBarrier1.current());
+            myBarrier2.await(myBarrier2.current());
+
+            if ((myRing1.getCurrentRing().size() == 6) && (myRing2.getCurrentRing().size() == 6))
+                break;
+        }
+
+        Assert.assertEquals(6, myRing1.getCurrentRing().size());
+        Assert.assertEquals(3, myRing1.getNeighbours().size());
+
+        Assert.assertEquals(6, myRing2.getCurrentRing().size());
+        Assert.assertEquals(3, myRing2.getNeighbours().size());
+
+        myPeer1.stop();
+        myPeer2.stop();
+    }
+
+    class StabiliserImpl implements ConsistentHash.Listener {
+        public void newNeighbour(ConsistentHash aRing, ConsistentHash.RingPosition anOwnedPosition,
+                                 ConsistentHash.RingPosition aNeighbourPosition) {
+            // Doesn't matter
+        }
+
+        public void rejected(ConsistentHash aRing, ConsistentHash.RingPosition anOwnedPosition) {
+            aRing.newPosition();
         }
     }
 }
